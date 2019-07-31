72465dd44c85fd047ff60a93de9ea7a98782db6f
nami izarra <nami.izarra@gmail.com>
2019-07-31 18:53:31 +0900

    refactored tasks into vue components and created template

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var app = new Vue({
   el: '#app',
+  components: {
+    'task': {
+              props: ['task'],
+              template: `
+              <div class="ui segment task" v-bind:class="task.completed ? 'done' : 'todo' ">
+                {{ task.id}} {{ task.name }}: {{ task.description }} completed? {{ task.completed}}
+              </div>
+              `
+            }
+  },
   data: {
     tasks: [
      { id: 1, name: 'Laundry', description: 'Wash clothes', completed: true},
